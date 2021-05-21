5cb83d5662c6550d8cef04a6c57e4b29af463a27
Tanay Shah <tanay.v.shah@gmail.com>
2021-05-20 21:00:36 -0700

    add units to controller

--- src/main/java/com/freekash/springboot/ApplicationController.java
+++ src/main/java/com/freekash/springboot/ApplicationController.java
@@ -7,4 +7,25 @@ public class ApplicationController {
     public String index(){
         return "index.html";
     }
+
+    @GetMapping("/misc")
+    public String misc(){
+        return "misc.html";
+    }
+
+    @GetMapping("/4")
+    public String iteration(){
+        return "4.html";
+    }
+
+    @GetMapping("/5")
+    public String classes(){
+        return "5.html";
+    }
+
+    @GetMapping("/6")
+    public String arrays(){
+        return "6.html";
+    }
+
 }
