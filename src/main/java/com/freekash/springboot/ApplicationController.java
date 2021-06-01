f7f3cd2b77184d2060d05d14e2743eb0745ead6e
Nathaniel Cherian <ngcherian2014@gmail.com>
2021-06-01 13:05:21 -0700

    thymeleaf fragments + navbar

--- src/main/java/com/freekash/springboot/ApplicationController.java
+++ src/main/java/com/freekash/springboot/ApplicationController.java
@@ -1,11 +1,19 @@
 package com.freekash.springboot;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@Controller
 public class ApplicationController {
     @GetMapping("/")
     public String index(){
         return "index.html";
+    }
+
+    @GetMapping("about")
+    public String about(){
+        return "about.html";
     }
 
     @GetMapping("/misc")
